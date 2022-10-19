11f026784139bd4620936e300591b679a4d1ec1c
NoushinTasnim <102020716+NoushinTasnim@users.noreply.github.com>
2022-10-20 00:45:22 +0600

    RecyclerView
    working!!!
    recyclerview and cardview

---
+++ app/src/main/java/com/example/tutorial/CustomAdapter.java
@@ -0,0 +1,75 @@
+package com.example.tutorial;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+    ArrayList<String> people;
+    ArrayList<String> occupation;
+    Context context;
+
+    public CustomAdapter(Context context, ArrayList<String> people, ArrayList<String> occupation) {
+        this.context = context;
+        this.people = people;
+        this.occupation = occupation;
+
+        Log.i("here","nopes");
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        Log.i("er","assdf");
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, parent, false);
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.person.setText(people.get(position));
+        holder.job.setText(occupation.get(position));
+
+        String p = people.get(position);
+
+        Log.i("hey", p);
+
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // display a toast with person name on item click
+                Toast.makeText(context, p, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return people.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView person, job;// init the item view's
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            Log.i("gt","as");
+            // get the reference of item view's
+            person = (TextView) itemView.findViewById(R.id.person);
+            job = (TextView) itemView.findViewById(R.id.occupation);
+        }
+    }
+}
+
--- app/src/main/java/com/example/tutorial/RecyclerViewActivity.java
+++ app/src/main/java/com/example/tutorial/RecyclerViewActivity.java
@@ -1,14 +1,54 @@
 package com.example.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    ArrayList arrayList = new ArrayList();
+    ArrayList arrayList2 = new ArrayList();
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        swipeRefreshLayout = findViewById(R.id.swipeContainer);
+
+        arrayList.add("Mr. X");
+        arrayList.add("Mrs. X");
+        arrayList.add("Mr. Y");
+        arrayList.add("Mr. Q");
+        arrayList.add("Mrs. L");
+        arrayList.add("Mrs. E");
+        arrayList.add("Mr. A");
+        arrayList.add("Mrs. Z");
+
+        arrayList2.add("Lawyer");
+        arrayList2.add("Realtor");
+        arrayList2.add("Car Dealer");
+        arrayList2.add("Painter");
+        arrayList2.add("Baker");
+        arrayList2.add("Fashion Designer");
+        arrayList2.add("Manager");
+        arrayList2.add("Scientist");
+
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        customAdapter = new CustomAdapter(this, arrayList, arrayList2);
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
     }
 }
