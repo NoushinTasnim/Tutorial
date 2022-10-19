f97b91b2b9aa37214c3b45fc2c9a5728944b8ae6
NoushinTasnim <102020716+NoushinTasnim@users.noreply.github.com>
2022-10-19 23:53:42 +0600

    button added in main
    can go to the next activity using button

--- app/src/main/java/com/example/tutorial/MainActivity.java
+++ app/src/main/java/com/example/tutorial/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RecyclerViewActivity.class));
+            }
+        });
     }
 }
